c9f5465aee70bcf9b349905b2b0193bbd8e58a92
brion <kidbrion7@gmail.com>
2020-10-12 11:59:04 +0300

    adding a home page to access all challenges

--- skull-emoji/script.js
+++ skull-emoji/script.js
@@ -1,16 +1,25 @@
 
+/*let emojis = [
+    "😉","🎈","☠","🎶","❤","💀","😒",
+    "🤣","👍","😁","😁","💕","😘",
+    "👌","😍","😂","😊",
+    "🙌","🤦‍♀️","🤦‍♂️","🤷","🤷‍♂️","✌","🤞","😎",
+    "🎂","🎉","🌹","💋","👏","😜","💖","😢",
+    "🤳","🐱‍🏍","😆","✨","🤔","🤢","🎁"
+] */
 function skullEmoji(){
     emoji = document.createElement("div");
     emoji.classList.add("skull-emoji");
-    emoji.innerText = "💀 ❤";
 
+    
+    emoji.innerText = "💀 ❤";
     emoji.style.left = Math.random()*100+"vw";
     emoji.style.animationDuration = Math.random()*10+2+3+"s";
 
     document.body.appendChild(emoji)
-    setTimeout(()=>{
-        skull-emoji.remove();
-    },5000);
+   // setTimeout(()=>{
+   //     emoji.remove();
+   // },1000);
 }
 
 setInterval(skullEmoji, 500);
